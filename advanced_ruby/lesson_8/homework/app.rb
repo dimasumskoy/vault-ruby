a5fb3246b73f21b2391e074ae059875737d23930
Dima <sumskoydima@gmail.com>
2019-02-14 00:41:40 +0600

    Add update/delete routes

--- advanced_ruby/lesson_8/homework/app.rb
+++ advanced_ruby/lesson_8/homework/app.rb
@@ -1,32 +1,61 @@
-require "sinatra"
+require "sinatra/base"
 require "sinatra/activerecord"
 require_relative "application"
 
-set :database, {
-  adapter: 'postgresql',
-  encoding: 'unicode',
-  database: 'final_task_app',
-  pool: 2,
-  username: 'postgres',
-  password: '34521'
-}
+class App < Sinatra::Application
+  set :method_override, true
+  set :database, {
+    adapter: 'postgresql',
+    encoding: 'unicode',
+    database: 'final_task_app',
+    pool: 2,
+    username: 'postgres',
+    password: '34521'
+  }
 
-get '/cars' do
-  @cars = Car.all
-  erb :cars
-end
+  get '/cars' do
+    @cars = Car.all
+    erb :cars
+  end
 
-get '/trucks' do
-  @trucks = Truck.all
-  erb :trucks
-end
+  get '/trucks' do
+    @trucks = Truck.all
+    erb :trucks
+  end
 
-post '/cars' do
-  @car = Car.create(params)
-  redirect :cars
-end
+  post '/cars' do
+    @car = Car.create(params)
+    redirect :cars
+  end
 
-post '/trucks' do
-  @truck = Truck.create(params)
-  redirect :trucks
+  post '/trucks' do
+    @truck = Truck.create(params)
+    redirect :trucks
+  end
+
+  get '/cars/:id' do
+    @car = Car.find(params[:id])
+    erb :car
+  end
+
+  # update resource
+  patch '/cars/:id' do
+    params.delete(:_method)
+
+    @car = Car.find(params[:id])
+    @car.update(params)
+
+    redirect :cars
+  end
+
+  delete '/cars/:id' do
+    params.delete(:_method)
+
+    @car = Car.find(params[:id])
+    @car.destroy
+
+    redirect :cars
+  end
+
+  run! if app_file == $0
 end
